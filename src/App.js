7a0ee46431fe75ddd01febc88f40f8bbaaaf88cf
Jose <u1541723@umail.utah.edu>
2026-01-30 18:17:23 -0700

    changed links distance for background particle animation

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
               links: {
                 enable: true,
                 color: '#003366',     // deep navy blue
-                distance: 500,
+                distance: 350,
                 opacity: 0.5          // more visible
               },
             },
